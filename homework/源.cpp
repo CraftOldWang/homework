e9ae6238aba4010d58d940dd2dc7d5153a2bc0ca
CraftOldWang <1053474528@qq.com>
2024-05-13 21:58:52 +0800

    s 5

--- homework/源.cpp
+++ homework/源.cpp
@@ -2,8 +2,13 @@
 #include<string>	
 #include<vector>
 
+//窗口参数
+const int WINDOW_WIDTH = 1280;
+const int WINDOW_HEIGHT = 720;
 
 inline void putimage_alpha(int x, int y, IMAGE* img);
+
+
 
 //动画...定义成一个类,因为loadimage只能加载单张图像
 class Animation
@@ -68,8 +73,49 @@ public:
 		delete anim_right;
 	}
 
+	//这里把while换成了switch,会出什么事吗?
+	//这里的switch不需要default吗????为什么下面enemy写了?
 	void ProcessEvent(const ExMessage&msg)
 	{
+		switch(msg.message)
+		{
+		case WM_KEYDOWN:
+			switch (msg.vkcode)
+			{
+			case VK_UP:
+				is_move_up = true;
+				break;
+			case VK_DOWN:
+				is_move_down = true;
+				break;
+			case VK_LEFT:
+				is_move_left = true;
+				break;
+			case VK_RIGHT:
+				is_move_right = true;
+				break;
+			}
+			break;
+			
+		case WM_KEYUP:
+			switch (msg.vkcode)
+			{
+			case VK_UP:
+				is_move_up = false;
+				break;
+			case VK_DOWN:
+				is_move_down = false;
+				break;
+			case VK_LEFT:
+				is_move_left = false;
+				break;
+			case VK_RIGHT:
+				is_move_right = false;
+				break;
+
+			}
+			break;
+		}
 
 	}
 
@@ -83,31 +129,54 @@ public:
 		{
 			double normalized_x = dir_x / len_dir;
 			double normalized_y = dir_y / len_dir;
-			position.x += (int)(PLAYER_SPEED * normalized_x);
-			position.y += (int)(PLAYER_SPEED * normalized_y);
+			position.x += (int)(SPEED * normalized_x);
+			position.y += (int)(SPEED * normalized_y);
 
 		}
 
 		if (position.x < 0)position.x = 0;
 		if (position.y < 0)position.y = 0;
-		if (position.x + PLAYER_WIDTH > WINDOW_WIDTH)position.x = WINDOW_WIDTH - PLAYER_WIDTH;
-		if (position.y + PLAYER_HEIGHT > WINDOW_HEIGHT)position.y = WINDOW_HEIGHT - PLAYER_HEIGHT;
+		if (position.x + FRAME_WIDTH > WINDOW_WIDTH)position.x = WINDOW_WIDTH - FRAME_WIDTH;
+		if (position.y + FRAME_HEIGHT > WINDOW_HEIGHT)position.y = WINDOW_HEIGHT - FRAME_HEIGHT;
 
 
 	}
 
+	//绘制玩家及阴影
 	void Draw(int delta)
 	{
+		//使阴影水平居中
+		int pos_shadow_x = position.x + (FRAME_WIDTH / 2 - SHADOW_WIDTH / 2);
+		//使阴影竖直方向在脚底下
+		int pos_shadow_y = position.y + FRAME_HEIGHT - 8;
+		putimage_alpha(pos_shadow_x, pos_shadow_y, &img_shadow);
 
+		static bool facing_left = false;
+		int dir_x = is_move_right - is_move_left;
+		if (dir_x < 0) 
+			facing_left = true; 
+		else if (dir_x > 0) 
+			facing_left = false; 
+
+		if (facing_left)
+			anim_left->Play(position.x, position.y, delta);
+		else
+			anim_right->Play(position.x, position.y, delta);
+		
+	}
+
+	POINT GetPosion()const
+	{
+		return position;
 	}
 
 	
 private:	//常量
 
-	const int PLAYER_WIDTH = 80;	//角色的宽
-	const int PLAYER_HEIGHT = 80;	//角色的高
+	const int FRAME_WIDTH = 80;		//角色的宽
+	const int FRAME_HEIGHT = 80;	//角色的高
 	const int SHADOW_WIDTH = 32;	//阴影的宽
-	const int PLAYER_SPEED = 3;	//角色速度
+	const int SPEED = 3;			//角色速度
 
 
 
@@ -123,23 +192,156 @@ private:	//变量
 
 };
 
+class Bullet
+{
+public:
+	POINT position = { 0,0 };
+
+public:
+	Bullet() = default;
+	~Bullet() = default;
+
+	void Draw() const
+	{
+		setlinecolor(RGB(255, 155, 50));
+		setfillcolor(RGB(200, 75, 10));
+		fillcircle(position.x, position.y, RADIUS);
+	}
+
+private:
+	const int RADIUS = 10;
+
+
+};
+
+class Enemy
+{
+public:
+	Enemy()
+	{
+		loadimage(&img_shadow, _T("img/shadow_enemy.png"));
+		anim_left = new Animation(_T("img/enemy_left_%d.png"), 6, 45);
+		anim_right = new Animation(_T("img/enemy_right_%d.png"), 6, 45);
+
+
+		//出生边界
+		enum class SpawnEdge
+		{
+			Up = 0,
+			Down,
+			Left,
+			Right,
+
+		};
+
+		SpawnEdge edge = (SpawnEdge)(rand() % 4);
+
+		switch (edge)
+		{
+		case SpawnEdge::Up:
+			position.x = rand() % WINDOW_WIDTH;
+			position.y = -FRAME_HEIGHT;
+			break;
+		case SpawnEdge::Down:
+			position.x = rand() % WINDOW_WIDTH;
+			position.y = WINDOW_HEIGHT;
+			break;
+		case SpawnEdge::Left:
+			position.x = - FRAME_WIDTH;
+			position.y = rand() % WINDOW_HEIGHT;
+			break;
+		case SpawnEdge::Right:
+			position.x =WINDOW_WIDTH;
+			position.y = rand() % WINDOW_HEIGHT;
+			break;
+		default:
+			break;
+		}
+
+	}
+
+	~Enemy()
+	{
+		delete anim_left;
+		delete anim_right;
+	}
+
+	bool CheckBulletCollision(const Bullet& bullet)
+	{
+	
+		return false;
+	}
+
+	bool CheckPlayerCollision(const Player& player)
+	{
+		
+		return false;
+	}
+
+	void Move(const Player& player)
+	{
+		int dir_x = player.GetPosion().x - position.x;
+		int dir_y = player.GetPosion().y - position.y;
+		double len_dir = sqrt(dir_x * dir_x + dir_y * dir_y);
+		//c++里^是位运算o,想要算幂的话,要么用pow,要么老老实实乘吧
+		if (len_dir != 0)
+		{
+			double normalized_x = dir_x / len_dir;
+			double normalized_y = dir_y / len_dir;
+			position.x += (int)(SPEED * normalized_x);
+			position.y += (int)(SPEED * normalized_y);
+
+		}
+
+		if (dir_x < 0)
+			facing_left = true;
+		else if (dir_x > 0)
+			facing_left = false;
+
+	}
+	
+
+	//绘制敌人及阴影
+	void Draw(int delta)
+	{
+		//使阴影水平居中
+		int pos_shadow_x = position.x + (FRAME_WIDTH / 2 - SHADOW_WIDTH / 2);
+		//使阴影竖直方向在脚底下;比较矮,所以要-35而不是-8
+		int pos_shadow_y = position.y + FRAME_HEIGHT - 35;
+		putimage_alpha(pos_shadow_x, pos_shadow_y, &img_shadow);
+
+
+		if (facing_left)
+			anim_left->Play(position.x, position.y, delta);
+		else
+			anim_right->Play(position.x, position.y, delta);
+
+	}
+
+
+private:	//常量
+	 
+	const int FRAME_WIDTH = 80;	//敌人的宽
+	const int FRAME_HEIGHT = 80;	//敌人的高
+	const int SHADOW_WIDTH = 48;	//阴影的宽
+	const int SPEED = 2;		//敌人速度
 
 
 
-//窗口参数
-const int WINDOW_WIDTH = 1280;
-const int WINDOW_HEIGHT = 720;
+private:	//变量
+	Animation* anim_left;
+	Animation* anim_right;
+	IMAGE img_shadow;
+	POINT position = { 0,0 };
+	bool facing_left = false;
 
 
-
-
-IMAGE img_background;
-
+};
 
 
 
 #pragma comment(lib,"MSIMG32.LIB")
-//透明部分变成透明而不会绘制出黑的
+//使得透明部分变成透明而不会绘制出黑的
 inline void putimage_alpha(int x, int y, IMAGE* img)
 {
 	int w = img->getwidth();
@@ -149,30 +351,17 @@ inline void putimage_alpha(int x, int y, IMAGE* img)
 
 }
 
-//绘制玩家及阴影
-void DrawPlayer(int delta, int dir_x)
+
+void TryGenerateEnemy(std::vector<Enemy*>& enemy_list)
 {
-	//使阴影水平居中
-	int pos_shadow_x = player_pos.x + (PLAYER_WIDTH / 2 - SHADOW_WIDTH / 2);
-	//使阴影竖直方向在脚底下
-	int pos_shadow_y = player_pos.y + PLAYER_HEIGHT - 8;
-	putimage_alpha(pos_shadow_x, pos_shadow_y, &img_shadow);
+	const int INTERVAL = 100;
+	static int counter = 0;
+	if ((++counter) % INTERVAL == 0)
+		enemy_list.push_back(new Enemy());
 
-	static bool facing_left = false;
-	 
-	if (dir_x < 0){	facing_left = true;}
-	else if (dir_x > 0)	{facing_left = false;}
-
-	if (facing_left)
-	{
-		anim_left_player.Play(player_pos.x, player_pos.y, delta);
-	}
-	else
-	{
-		anim_right_player.Play(player_pos.x, player_pos.y, delta);
-
-	}
 }
+
+
 
 int main()
 {
@@ -181,16 +370,12 @@ int main()
 
 	bool running = true;
 
+	Player player;
 	ExMessage msg;
-	
-
-
-
-
+	IMAGE img_background;
+	std::vector<Enemy*> enemy_list;
 	
 	loadimage(&img_background, _T("img/background.png"));
-
-
 
 	BeginBatchDraw();
 
@@ -201,56 +386,31 @@ int main()
 		//读取数据
 		while (peekmessage(&msg))
 		{
-			if (msg.message == WM_KEYDOWN)
-			{
-				switch (msg.vkcode)
-				{
-				case VK_UP:
-					is_move_up=true;
-					break;
-				case VK_DOWN:
-					is_move_down = true;
-					break;
-				case VK_LEFT:
-					is_move_left = true;
-					break;
-				case VK_RIGHT:
-					is_move_right = true;
-					break;
-
-				}
-			}
-			else if(msg.message == WM_KEYUP)
-			{
-				switch (msg.vkcode)
-				{
-				case VK_UP:
-					is_move_up = false;
-					break;
-				case VK_DOWN:
-					is_move_down = false;
-					break;
-				case VK_LEFT:
-					is_move_left = false;
-					break;
-				case VK_RIGHT:
-					is_move_right = false;
-					break;
-
-				}
-			}
-
-
-
+			player.ProcessEvent(msg);
 		}
 
 
-*
+		//处理数据
+		player.Move();
+		TryGenerateEnemy(enemy_list);
+		for (Enemy* enemy : enemy_list)
+			enemy->Move(player);
+
+
+
+
+
 		//绘制
 		cleardevice();
+		//似乎谁先绘制谁就会被后绘制的覆盖>如果有重叠的话
 
 		putimage(0, 0, &img_background);
-		DrawPlayer(1000 / 144, is_move_right - is_move_left);
+		player.Draw(1000 / 144);
+		for (Enemy* enemy : enemy_list)
+			enemy->Draw(1000/144);
+
+
+
 		   
 		FlushBatchDraw();
 
